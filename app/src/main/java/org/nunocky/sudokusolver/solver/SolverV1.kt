128f23b4eecbb8f10d981fe66e4d79368d30dabc
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2021-07-08 20:11:01 +0900

    Fix std solver (#53)
    * 標準ソルバー修正中
    
    フォルタ処理をクラスに切り分ける
    
    * コメント追加

--- app/src/main/java/org/nunocky/sudokusolver/solver/SolverV1.kt
+++ app/src/main/java/org/nunocky/sudokusolver/solver/SolverV1.kt
@@ -11,103 +11,155 @@ class SolverV1(
 
     private fun isSolved() = parent.isSolved()
 
-    override fun trySolve(): Boolean {
-        for (difficulty in 1..3) {
+    private val basicFilters = ArrayList<SudokuFilter>()
+    private val mediumFilters = ArrayList<SudokuFilter>()
+    private val hardFilters = ArrayList<SudokuFilter>()
 
-            if (isSolved()) {
-                break
+    init {
+        for (cell in cells) {
+            basicFilters.add(Filter0(this, cell))
+        }
+
+        for (cell in cells) {
+            basicFilters.add(FilterOneCandidate(this, cell))
+        }
+
+        for (group in groups) {
+            basicFilters.add(FilterLastOneCellInGroup(this, group))
+        }
+
+        for (group in groups) {
+            for (n in 2..7) {
+                mediumFilters.add(FilterCombination(this, group, n))
             }
+        }
 
-            parent.difficulty = difficulty + SudokuSolver.DIFFICULTY_UNDEF
+        // TODO add hard filters
+        // valueChanged = valueChanged or filterGroupCellX2(cell.groups)
+    }
+
+    // TODO : 不正な解析を行ったら例外を発生させる
+    override fun trySolve(): Boolean {
+
+        for (difficulty in 0..2) {
+            parent.difficulty = difficulty + SudokuSolver.DIFFICULTY_EASY
 
             var valueChanged = true
 
             while (!isSolved() && valueChanged) {
                 valueChanged = false
 
+                var shouldRepeat: Boolean
+
                 // EASY
-                cells.forEach { cell ->
-                    valueChanged = valueChanged or filter0(cell)
-                }
-                callback?.onProgress(cells)
+                shouldRepeat = true
+                do {
+                    for (filter in basicFilters) {
+                        shouldRepeat = filter.exec()
+                        valueChanged = valueChanged or shouldRepeat
 
-                cells.forEach { cell ->
-                    valueChanged = valueChanged or filterOneCandidate(cell)
-                }
-//                callback?.onProgress(cells)
-
-                groups.forEach {
-                    valueChanged = valueChanged or filterLastOneCellInGroup(it)
-                }
-//                callback?.onProgress(cells)
+                        callback?.onProgress(cells)
+                        if (!parent.calcIsValid()) {
+                            return false
+                        }
+                    }
+                } while (shouldRepeat)
 
                 // MEDIUM
-                if (1 < difficulty) {
-                    groups.forEach { g ->
-                        valueChanged = valueChanged or filterGroupCellX(g)
-                    }
-                }
-//                callback?.onProgress(cells)
+                if (0 < difficulty) {
+                    for (filter in mediumFilters) {
+                        valueChanged = valueChanged or filter.exec()
 
-                // HARD : Combination filter
-                if (2 < difficulty) {
-                    for (group in groups) {
-                        for (n in 2..4) {
-                            valueChanged = valueChanged or filterCombination(group, n)
+                        callback?.onProgress(cells)
+                        if (!parent.calcIsValid()) {
+                            return false
                         }
                     }
                 }
 
-                callback?.onProgress(cells)
+                // HARD
+                if (1 < difficulty) {
+                    for (filter in hardFilters) {
+                        valueChanged = valueChanged or filter.exec()
 
-                // 解析に間違いがあったら停止
-                if (!parent.calcIsValid()) {
-                    return false
+                        callback?.onProgress(cells)
+                        if (!parent.calcIsValid()) {
+                            return false
+                        }
+                    }
+                }
+
+                if (isSolved()) {
+                    return true
                 }
             }
 
-            if (isSolved()) {
-                return true
-            }
         }
 
         return isSolved()
     }
+}
 
-    /**
-     * あるセルの要素が確定しているとき、そのセルが属するすべてのグループでそのセルの値は候補から外れる
-     */
-    private fun filter0(cell: Cell): Boolean {
+/**
+ * フィルタの基本クラス
+ */
+private abstract class SudokuFilter(protected val parent: SolverV1) {
+    abstract fun exec(): Boolean
+}
+
+/**
+ * 対象セルの値が nに確定していたら、そのセルが所属する他グループの未確定セルから要素 nを削除する
+ */
+private class Filter0(parent: SolverV1, private val cell: Cell) : SudokuFilter(parent) {
+    override fun exec(): Boolean {
         var changed = false
 
-        if (cell.isFixed) {
-            changed = changed or sweepCandidateForCell(cell, cell.value)
-        }
+        var shouldRepeat: Boolean
+        do {
+            shouldRepeat = false
+            if (cell.isFixed) {
+                val n = cell.value
+                cell.groups.forEach { g ->
+                    g.cells.forEach { c ->
+                        if (c.candidates.contains(n)) {
+                            c.candidates.remove(n)
+                            changed = true
+                        }
 
+                        // そのセルの候補が残り1だったらそのセルは確定。そのときはもう一度このフィルタを繰り返す
+                        if (c.candidates.size == 1) {
+                            c.value = c.candidates.first()
+                            shouldRepeat = true
+                        }
+                    }
+                }
+            }
+        } while (shouldRepeat)
         return changed
     }
+}
 
-    /**
-     * あるセルについて、候補が一つだけのときは値が確定する
-     */
-    private fun filterOneCandidate(cell: Cell): Boolean {
+/**
+ * あるセルについて、候補が一つだけのときは値が確定する
+ */
+private class FilterOneCandidate(parent: SolverV1, private val cell: Cell) : SudokuFilter(parent) {
+    override fun exec(): Boolean {
         var changed = false
 
         if (cell.candidates.size == 1) {
-            val fixedNum = cell.candidates.first()
-            cell.value = fixedNum
-            sweepCandidateForCell(cell, fixedNum)
-
+            cell.value = cell.candidates.first()
             changed = true
         }
-
         return changed
     }
+}
 
-    /**
-     * あるグループについて、グループ内で未確定のセルが一つだけのとき、そのセルの値は確定する
-     */
-    private fun filterLastOneCellInGroup(group: Group): Boolean {
+/**
+ * あるグループについて、グループ内で未確定のセルが一つだけのとき、そのセルの値は確定する
+ */
+private class FilterLastOneCellInGroup(parent: SolverV1, private val group: Group) :
+    SudokuFilter(parent) {
+    override fun exec(): Boolean {
         var changed = false
 
         var unFixedCell: Cell? = null
@@ -125,41 +177,21 @@ class SolverV1(
             unFixedCell?.let { cell ->
                 val fixedNum = candidates.first()
                 cell.value = fixedNum
-                sweepCandidateForCell(cell, fixedNum)
                 changed = true
             }
         }
 
         return changed
     }
+}
 
-    /**
-     * あるグループにおいて、 候補 nを持つセルが1つしかない → そのセルの値は nで確定する
-     */
-    private fun filterGroupCellX(group: Group): Boolean {
-        var changed = false
-
-        for (n in 1..9) {
-            val tmpCells = group.cells.filter {
-                it.candidates.contains(n)
-            }
-
-            if (tmpCells.size == 1) {
-                val c = tmpCells.first()
-                c.value = n
-                changed = changed or sweepCandidateForCell(c, n)
-                changed = true
-            }
-        }
-
-        return changed
-    }
-
-    /**
-     *  あるグループにおいて、同一の n個の候補を持つセルが n個存在するなら、それら
-     *  のセルでその候補値を専有できる → グループ内のそれら以外のセルで、候補値は除外できる
-     */
-    private fun filterCombination(group: Group, n: Int): Boolean {
+/**
+ *  あるグループにおいて、同一の n個の候補を持つセルが n個存在するなら、それら
+ *  のセルでその候補値を専有できる → グループ内のそれら以外のセルで、候補値は除外できる
+ */
+private class FilterCombination(parent: SolverV1, private val group: Group, private val n: Int) :
+    SudokuFilter(parent) {
+    override fun exec(): Boolean {
         var changed = false
 
         for (i in 0 until 9) {
@@ -196,32 +228,36 @@ class SolverV1(
                         changed = true
                     }
                 }
-
-                changed = changed or filterOneCandidate(cell)
-            }
-        }
-
-        return changed
-    }
-
-    /**
-     * cellの所属するグループに対して、すべてのセルから候補 nを取り除く
-     */
-    private fun sweepCandidateForCell(cell: Cell, n: Int): Boolean {
-        var changed = false
-        for (g in cell.groups) {
-            for (c in g.cells) {
-                if (c == cell) {
-                    continue
-                }
-
-                if (c.candidates.contains(n)) {
-                    c.candidates.remove(n)
-                    changed = true
-                }
             }
         }
 
         return changed
     }
 }
+
+// /**
+// * あるセルに対して、関連する3つのグループにおいて
+// *   + 候補 nを持つセルが1つだけ
+// * が共通している
+// */
+//private class FilterGroupCellX2(parent: SolverV1, private val cell: Cell) : SudokuFilter(parent) {
+//    override fun exec(): Boolean {
+//        var changed = false
+//
+//        val ary = IntArray()
+//        for (g in cell.groups) {
+//            for (n in 1..9) {
+//                val tmpCells = groups.flatMap { it.cells }
+//
+//                if (tmpCells.size == 1) {
+//                    val c = tmpCells.first()
+//                    fixCell(c, n)
+//                    changed = true
+//                    ary.put(n)
+//                }
+//            }
+//        }
+//
+//        return changed
+//    }
+//}
