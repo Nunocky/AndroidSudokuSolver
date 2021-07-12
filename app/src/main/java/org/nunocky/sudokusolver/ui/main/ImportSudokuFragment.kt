78fd4c631c3ef67c99d266e04f0ff1a88bc76f42
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2021-07-12 23:02:31 +0900

    JSONファイルの import機能
    失敗したらテキストとして扱う

--- app/src/main/java/org/nunocky/sudokusolver/ui/main/ImportSudokuFragment.kt
+++ app/src/main/java/org/nunocky/sudokusolver/ui/main/ImportSudokuFragment.kt
@@ -1,9 +1,7 @@
 package org.nunocky.sudokusolver.ui.main
 
 import android.app.Activity
-import android.app.Application
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,57 +12,11 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.nunocky.sudokusolver.MyApplication
-import org.nunocky.sudokusolver.database.SudokuEntity
 import org.nunocky.sudokusolver.database.SudokuRepository
 import org.nunocky.sudokusolver.databinding.FragmentImportSudokuBinding
-
-class ImportSudokuViewModel(
-    application: Application,
-    private val repository: SudokuRepository
-) : AndroidViewModel(application) {
-    class Factory(private val application: Application, private val repository: SudokuRepository) :
-        ViewModelProvider.NewInstanceFactory() {
-        @Suppress("unchecked_cast")
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return ImportSudokuViewModel(application, repository) as T
-        }
-    }
-
-    suspend fun execImport(uri: Uri) = withContext(Dispatchers.IO) {
-        val app = getApplication() as MyApplication
-        app.contentResolver.openInputStream(uri).use { oStream ->
-            oStream?.bufferedReader()?.use { reader ->
-
-                val list = ArrayList<SudokuEntity>()
-
-                var line: String? = ""
-                do {
-                    line = reader.readLine()?.trim()
-                    if (line?.length == 81) {
-                        list.add(
-                            SudokuEntity(
-                                id = 0,
-                                cells = line,
-                                difficulty = 1
-
-                            )
-                        )
-                    }
-                } while (line?.isNotEmpty() == true)
-
-                repository.insert(list)
-            }
-        }
-    }
-}
 
 class ImportSudokuFragment : Fragment() {
     private lateinit var binding: FragmentImportSudokuBinding
@@ -92,8 +44,7 @@ class ImportSudokuFragment : Fragment() {
 
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
-                type = "text/plain"
-                putExtra(Intent.EXTRA_TITLE, "sudoku_backup.txt")
+                type = "*/*"
             }
             startForResult.launch(intent)
         }
