dc01dc0cefb201b181d914dec4e177a764d38309
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2023-06-03 15:18:33 +0900

    DIFFICULTYのDBでの管理方法修正
    CataConverter に Int との相互変換を追加

--- app/src/main/java/org/nunocky/sudokusolver/database/SudokuEntity.kt
+++ app/src/main/java/org/nunocky/sudokusolver/database/SudokuEntity.kt
@@ -1,8 +1,11 @@
 package org.nunocky.sudokusolver.database
 
+import androidx.databinding.adapters.Converters
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import org.nunocky.sudokulib.DIFFICULTY
 import se.ansman.kotshi.JsonSerializable
 import java.util.*
@@ -13,10 +16,9 @@ data class SudokuEntity(
     @PrimaryKey(autoGenerate = true) var id: Long = 0L,
     var cells: String = "0".repeat(81),
     var createdAt: Calendar = Calendar.getInstance(),
-    var difficulty: DIFFICULTY? = DIFFICULTY.UNDEF, // "UNTESTED"
+    var difficulty: DIFFICULTY = DIFFICULTY.UNDEF,
     var thumbnail: String? = "",
 
     @Ignore
     var isChecked: Boolean = false
 )
-
