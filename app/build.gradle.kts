9d9abec29922a42693ffdf8c2a6df3b1f4184ec7
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-08-16 18:50:47 +0900

    compileSdk = 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "org.nunocky.sudokusolver"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.nunocky.sudokusolver"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "1.0"
 
--- sudokulib/build.gradle.kts
+++ sudokulib/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "org.nunocky.sudokulib"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
