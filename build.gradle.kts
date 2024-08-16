74705c7f6466e42bb99f4d3564a43309450449fe
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2024-08-16 18:24:36 +0900

    buildscriptの部分も libs.versions.tomlで管理

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlin_version = "1.9.0"
-        classpath("com.android.tools.build:gradle:8.1.4") // TODO libs.versions.tomlで定義
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version") // TODO libs.versions.tomlで定義
+        classpath(libs.plugins.android.tools.build.gradle.get().toString())
+        classpath(libs.plugins.jetbrains.kotlin.gradle.get().toString())
+        classpath(libs.plugins.dagger.hilt.android.gradle.get().toString())
+        classpath(libs.plugins.androidx.navigation.safe.args.get().toString())
+        classpath(libs.plugins.gms.oss.licenses.get().toString())
 
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
